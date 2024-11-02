f8e2055d2bb8422b0f59df874fb540cca5585da8
Rama Hisyam Alchoiri <ramahisyam1@gmail.com>
2024-11-02 10:56:12 +0700

    complete before submitting

--- src/routes.js
+++ src/routes.js
@@ -1,31 +1,31 @@
-const { getAllNotesHandler, addBookHandler } = require('./handler');
+const { addBookHandler, getAllBooksHandler, getBookByIdHandler, editBookByIdHandler, deleteBookByIdHandler } = require('./handler');
 
 const routes = [
     {
       method: 'POST',
-      path: '/notes',
+      path: '/books',
       handler: addBookHandler,
     },
     {
       method: 'GET',
-      path: '/notes',
-      handler: getAllNotesHandler,
+      path: '/books',
+      handler: getAllBooksHandler,
     },
-    // {
-    //   method: 'GET',
-    //   path: '/notes/{id}',
-    //   handler: getNoteByIdHandler,
-    // },
-    // {
-    //   method: 'PUT',
-    //   path: '/notes/{id}',
-    //   handler: editNoteByIdHandler,
-    // },
-    // {
-    //   method: 'DELETE',
-    //   path: '/notes/{id}',
-    //   handler: deleteNoteByIdHandler,
-    // },
+    {
+      method: 'GET',
+      path: '/books/{id}',
+      handler: getBookByIdHandler,
+    },
+    {
+      method: 'PUT',
+      path: '/books/{id}',
+      handler: editBookByIdHandler,
+    },
+    {
+      method: 'DELETE',
+      path: '/books/{id}',
+      handler: deleteBookByIdHandler,
+    },
   ];
 
   module.exports = routes;
